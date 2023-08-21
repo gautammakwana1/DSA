59eef00f00ffb86aab3771fd3d7d0dcd3d6e216c
Gautam Makwana <139319282+gautammakwana1@users.noreply.github.com>
2023-08-21 20:20:20 +0530

    21/08 Home

--- stack.c
+++ stack.c
@@ -1,51 +1,122 @@
 #include<stdio.h>
-#define n 5
+#define n 10
 int a[n];
 int i,top=-1;
 int display()
 {
     if(top<0)
     {
-        printf("Array is empty...");
+        printf("Array is empty. ");
     }
-    else
-    {
+    else{
         for(i=0;i<=top;i++)
         {
             printf("%d ",a[i]);
         }
     }
 }
+int insertFirst(int val)
+{
+    if(top>n-1)
+    {
+        printf("Array is already full. ");
+    }
+    else{
+        for(i=top;i>=0;i--)
+        {
+            a[i+1]=a[i];
+        }
+        a[0]=val;
+        top++;
+    }
+}
+int deleteFirst()
+{
+    if(top<0)
+    {
+        printf("Array is already emty. ");
+    }
+    else{
+        int p;
+        for(i=0;i<top;i++)
+        {
+            a[i]=a[i+1];
+        }
+        top--;
+        return p;
+    }
+}
 int insertEnd(int val)
 {
-    if(top>=n-1)
+    if(top>n-1)
     {
-        printf("Array is full \n");
+        printf("Array is already full. ");
     }
     else{
         top++;
         a[top]=val;
     }
 }
-int delete()
+int deleteEnd()
 {
     if(top<0)
     {
-        printf("Array is already empty");
+        printf("Array is already empty. ");
     }
     else{
+        int p =a[top];
+        // for(i=top;i>0;i--)
+        // {
+        //     a[i+1]=a[i];
+        // }
         top--;
-        //a[top]=val;
+        return p;
     }
 }
+
 int main()
 {
-    insertEnd(100);
-    insertEnd(200);
-    insertEnd(300);
-    insertEnd(400);
-    insertEnd(500);
-    delete();
-    delete();
-    display();
+    int ch;
+    printf("1. Display ");
+    printf("\n2. Insert Array First ");
+    printf("\n3. Delete Array First ");
+    printf("\n4. Insert Array End ");
+    printf("\n5. Delete Array End ");
+    printf("\n0. End Program ");
+    do
+    {
+        printf("\nEnter Your Choice :");
+        scanf("%d",&ch);
+        int val;
+        switch(ch)
+        {
+            case 1:
+                    display();
+                    break;
+            case 2:
+                    printf("Enter your array :");
+                    scanf("%d",&val);
+                    insertFirst(val);
+                    break;
+            case 3:
+                    deleteFirst();
+                    break;
+            case 4:
+                    printf("Enter your array :");
+                    scanf("%d",&val);
+                    insertEnd(val);
+                    break;
+            case 5:
+                    deleteEnd();
+                    break;
+            case 0:
+                    printf("Program is closed .");
+                    break;
+            default:
+                    printf("You Enter Wrong choice .");
+                    break;
+        }
+    }while(ch!=0);
+
+    return 0;
 }
